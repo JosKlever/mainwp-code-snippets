091dc799283c7355b486789e9f10d113757f06ce
Jos Klever <josklever@gmail.com>
2019-01-03 23:13:09 +0100

    Create show-ghost-updates.php

---
+++ show-ghost-updates.php
@@ -0,0 +1,24 @@
+$output = "";
+
+// Check plugins
+$plugin_updates = get_site_transient( 'update_plugins' );
+if ( $plugin_updates && ! empty( $plugin_updates->response ) ) {
+  foreach ( $plugin_updates->response as $plugin => $details ) {
+    $title = $details->title;
+    $output .= "<p><strong>Plugin</strong> <u>$title</u> ($plugin) is reporting an available update.</p>";
+  }
+}
+
+// Check themes
+wp_update_themes();
+$theme_updates = get_site_transient( 'update_themes' );
+if ( $theme_updates && ! empty( $theme_updates->response ) ) {
+  foreach ( $theme_updates->response as $theme => $details ) {
+    $title = $details->title;
+    $output .= "<p><strong>Theme</strong> <u>$title</u> ($theme) is reporting an available update.</p>";
+  }
+}
+
+if ( empty( $output ) ) $output = "No pending updates found in the database.";
+
+echo "<h2>Pending updates</h2>" . $output;
